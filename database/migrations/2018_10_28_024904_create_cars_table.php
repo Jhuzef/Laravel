379079efe25bafcd5ae2d76dec38fd21d8fe948e
jhuzef <josephgonzales034@gmail.com>
2018-10-27 20:23:50 -0700

    Task: Create a database seed to generate 50 cars

--- database/migrations/2018_10_28_024904_create_cars_table.php
+++ database/migrations/2018_10_28_024904_create_cars_table.php
@@ -15,6 +15,7 @@ class CreateCarsTable extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('Make');
             $table->timestamps();
         });
     }
